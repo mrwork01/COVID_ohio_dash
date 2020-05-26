84b730f825aa778c2f141008c6ccb78542e215a9
mrwork01 <shifter38j@yahoo.com>
2020-05-26 07:27:26 -0400

    5/26 Update

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ app.layout = html.Div([
 					html.Label('''Disclaimer:  Data is not in real time.  Due to the nature of the situation and manner of
 								collection, data may lag several days.'''),
 					html.Br(),
-					html.Label('''*Last Updated: 5/21/20 6:10 AM EST''')]),
+					html.Label('''*Last Updated: 5/26/20 6:10 AM EST''')]),
 
 				html.Br(),
 				html.Div([
